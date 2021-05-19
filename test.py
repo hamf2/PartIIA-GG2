30178b85a0ef83b0f007667b69f917e10f3d5f1b
Harry Fieldhouse <harryfieldhouse@gmail.com>
2021-05-19 11:30:07 +0100

    Tidied up test implementation

--- test.py
+++ test.py
@@ -11,24 +11,27 @@ from create_dicom import *
 material = Material()
 source = Source()
 
-def test_1(nmetal, n, scale):
-    # initial conditions: circle of material 'nmetal' scanned by fake ideal source
-    p = ct_phantom(material.name, n, 1, metal=nmetal)
+def test_1():
+    # initial conditions: circle of material scanned by fake ideal source
+    # set test size and scale
+    n, scale = 128, 0.01
     s = fake_source(source.mev, 0.1, method='ideal')
-    y = scan_and_reconstruct(s, material, p, scale, n)
 
-	# save some meaningful results
+    with open('test_output/test_1_output.txt', mode='w') as f:
+        f.write('Test with image size: ' +str(n)+'x'+str(n)+ ' and scale: ' +str(scale)+ ' cm/pixel\n\n')
 
-    o_str = '{0:<35} {1:<20} {2:>12}'.format('Mean value for ' + nmetal + ' is ', str(np.mean(y[n//4:3*n//4, n//4:3*n//4])),
-     '(Expected: ' + str(material.coeffs[material.name.index(nmetal)][np.argmax(s)] - material.coeffs[0][np.argmax(s)]) + ')\n')
-    with open('test_output/test_1_output.txt', mode='a') as f:
-        f.write(o_str)
+        for nmetal in material.name[:9]:
+            p = ct_phantom(material.name, n, 1, metal=nmetal)
+            y = scan_and_reconstruct(s, material, p, scale, n)
 
-# set test size and scale
-n, scale = 128, 0.01
-# create/wipe output file and run tests
+	        # save some meaningful results
+
+            o_str = '{0:<35} {1:<20} {2:>12}'.format('Mean value for ' + nmetal + ' is ', str(np.mean(y[n//4:3*n//4, n//4:3*n//4])),
+             '(Expected: ' + str(material.coeffs[material.name.index(nmetal)][np.argmax(s)] - material.coeffs[0][np.argmax(s)]) + ')\n')
+            f.write(o_str)
+
+
+# create output directory and run tests
 if(not(os.path.exists('test_output'))):os.mkdir('test_output')
-with open('test_output/test_1_output.txt', mode='w') as f:
-    f.write('Test with image size: ' +str(n)+'x'+str(n)+ ' and scale: ' +str(scale)+ ' cm/pixel\n\n')
-for nmetal in material.name[:9]:
-    test_1(nmetal, n, scale)
+print('Test 1')
+test_1()
