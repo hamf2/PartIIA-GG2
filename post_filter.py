a62e20d68e5cdc850e289a200416116d93dbb271
Luke Gatecliff <lg570@cam.ac.uk>
2021-06-06 09:41:51 +0100

    Added checks to post_filter

--- post_filter.py
+++ post_filter.py
@@ -10,37 +10,67 @@ def post_filter(image, filter):
         denoise - remove noise by Gaussian blur followed by median averaging
         close - flatten image by grey dilation/erosion
         edge - apply the Sobel operator to detect edges
-        unsharp - add overshoot to edges by unsharp masking"""
-
-    if filter is 'denoise':
-
-        # remove noise by gaussian blur followed by median averaging
-        blurred = ndimage.gaussian_filter(image, 3)
-        return ndimage.median_filter(image, 5).astype('int')
-
-    elif filter is 'close':
+        unsharp - add overshoot to edges by unsharp masking
         
-        # flatten image by grey dilation/erosion
-        return ndimage.grey_closing(image, (5, 5)).astype('int')
+        Multistage filtering can be applied by setting filter to a list of 
+        filters to be applied"""
 
-    elif filter is 'edge':
+    # check image type is broadcastable to array
+    if not isinstance(image, np.ndarray):
+        try:
+            img = np.array(image)
+        except:
+            raise TypeError('image must be a 2D numpy array or broadcastable to a 2D array')
+    else:
+        img = np.copy(image)
 
-        # apply the Sobel operator to detect edges
-        sx = ndimage.sobel(image, axis=0, mode='constant')
-        sy = ndimage.sobel(image, axis=1, mode='constant')
-        edge = np.hypot(sx, sy)
-        return np.interp(edge, (edge.min(), edge.max()), (-1024, 3071)).astype('int')
+    # check image is 2 dimensional
+    if len(img.shape) != 2:
+        raise TypeError('image must be a 2D array')
 
-    elif filter is 'unsharp':
+    # apply multistage filtering
+    if isinstance(filter, (list, tuple)):
 
-        # add overshoot to edges by unsharp masking
-        blurred = ndimage.gaussian_filter(image, 3)
-        filter_blurred = ndimage.gaussian_filter(blurred, 1)
-        unsharp = blurred + 30 * (blurred - filter_blurred)
-        return np.interp(unsharp, (unsharp.min(), unsharp.max()), (-1024, 3071)).astype('int')
+        # apply each filter in turn
+        for subfilter in filter:
+            img = post_filter(img, subfilter)
+        return img
+
+    # apply filter
+    elif isinstance(filter, str):
+        if filter is 'denoise':
+
+            # remove noise by gaussian blur followed by median averaging
+            blurred = ndimage.gaussian_filter(img, 3)
+            return ndimage.median_filter(blurred, 5).astype('int')
+
+        elif filter is 'close':
+            
+            # flatten image by grey dilation/erosion
+            return ndimage.grey_closing(img, (5, 5)).astype('int')
+
+        elif filter is 'edge':
+
+            # apply the Sobel operator to detect edges
+            sx = ndimage.sobel(img, axis=0, mode='constant')
+            sy = ndimage.sobel(img, axis=1, mode='constant')
+            edge = np.hypot(sx, sy)
+            return np.interp(edge, (edge.min(), edge.max()), (-1024, 3071)).astype('int')
+
+        elif filter is 'unsharp':
+
+            # add overshoot to edges by unsharp masking
+            blurred = ndimage.gaussian_filter(img, 3)
+            filter_blurred = ndimage.gaussian_filter(blurred, 1)
+            unsharp = blurred + 30 * (blurred - filter_blurred)
+            return np.interp(unsharp, (unsharp.min(), unsharp.max()), (-1024, 3071)).astype('int')
+
+        else:
+
+            # raise error for invalid filter
+            raise ValueError("filter '{}' not available. Possible filters are: 'denoise', 'close', 'edge' and 'unsharp'".format(filter))
 
     else:
 
-        # return image for invalid filter
-        print("Invalid filter, filters available are: 'denoise', 'close', 'edge' and 'unsharp'")
-        return image
+        # raise error for invalid filter
+        raise TypeError('filter must be a string or list of strings')
